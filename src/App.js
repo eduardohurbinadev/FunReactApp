4dd6ef04591beb93006b00430a536d3617c66aa4
Eduardo Urbina Dev <eduardoh.urbina.developer@gmail.com>
2019-11-05 23:05:25 -0600

    Search bar is now working and fetching data from the Github API Users

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users'; 
+import Search from './components/users/Search'; 
 import axios from 'axios'
 
 class App extends Component {
@@ -20,6 +21,14 @@ class App extends Component {
 
   }
 
+  //Search Github users 
+    searchUsers = async text => {
+
+      const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+      this.setState({ users: res.data.items, loading: false }); 
+      }
+
   //LifeCycle Method which is actually required
   render() {
 
@@ -28,6 +37,7 @@ class App extends Component {
         {/* //Props are properties that you can pass in to a component */}
         <Navbar/>
         <div className="container">
+          <Search searchUsers={this.searchUsers}/>
         <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
