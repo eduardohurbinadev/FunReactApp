46f929a7b185dbffe1e55eacecafaf4785367d09
Eduardo Urbina Dev <eduardoh.urbina.developer@gmail.com>
2019-11-06 18:28:37 -0600

    Added alerts for the Search bar

--- src/App.js
+++ src/App.js
@@ -1,48 +1,68 @@
-import React, { Component } from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users'; 
-import Search from './components/users/Search'; 
-import axios from 'axios'
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
+import axios from "axios";
 
 class App extends Component {
-  state = {
-    users: [],
-    loading: false
+	state = {
+		users: [],
+    loading: false,
+    alert: null
+	};
+
+	//LifeCycle Method
+	async componentDidMount() {
+		this.setState({ loading: true });
+
+		const res = await axios.get(
+			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({ users: res.data, loading: false });
+	}
+
+	//Search Github users
+	searchUsers = async text => {
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({ users: res.data.items, loading: false });
+	};
+
+	// Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+  
+  // Set alert 
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type }}); 
+    setTimeout(()=> this.setState({ alert: null }), 3000); 
   }
 
-  //LifeCycle Method 
-  async componentDidMount() {
-    this.setState({ loading: true }); 
+	//LifeCycle Method which is actually required
+	render() {
+    const { users, loading } = this.state; 
 
-    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-    this.setState({ users: res.data, loading: false }); 
-
-  }
-
-  //Search Github users 
-    searchUsers = async text => {
-
-      const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-
-      this.setState({ users: res.data.items, loading: false }); 
-      }
-
-  //LifeCycle Method which is actually required
-  render() {
-
-    return (
-      <div className="App">
-        {/* //Props are properties that you can pass in to a component */}
-        <Navbar/>
-        <div className="container">
-          <Search searchUsers={this.searchUsers}/>
-        <Users loading={this.state.loading} users={this.state.users} />
-        </div>
-      </div>
-    );
-  }
+		return (
+			<div className='App'>
+				{/* //Props are properties that you can pass in to a component */}
+				<Navbar />
+				<div className='container'>
+          <Alert alert={this.state.alert}/>
+					<Search
+						searchUsers={this.searchUsers}
+						clearUsers={this.clearUsers}
+						showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+            />
+					<Users loading={loading} users={users} />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
 export class Search extends Component {
 	state = {
 		text: ""
 	};
 
-    static propTypes = {
-        searchUsers: PropTypes.func.isRequired,
-    }
+	static propTypes = {
+		searchUsers: PropTypes.func.isRequired,
+		clearUsers: PropTypes.func.isRequired,
+		showClear: PropTypes.bool.isRequired,
+		setAlert: PropTypes.func.isRequired
+	};
 
-    onChange = (e) => this.setState({ [e.target.name]: e.target.value});
-    
-    onSubmit = e => {
-        e.preventDefault();
-        this.props.searchUsers(this.state.text);
-        this.setState({ text: '' }); 
-    }
+	onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+	onSubmit = e => {
+		e.preventDefault();
+		if (this.state.text === "") {
+			this.props.setAlert("Search bar cannot be in blank", "light");
+		} else {
+			this.props.searchUsers(this.state.text);
+			this.setState({ text: "" });
+		}
+	};
 
 	render() {
+		const { showClear, clearUsers } = this.props;
+
 		return (
 			<div>
 				<form onSubmit={this.onSubmit} className='form'>
@@ -26,8 +35,8 @@ export class Search extends Component {
 						type='text'
 						name='text'
 						placeholder='Search Users...'
-                        value={this.state.text}
-                        onChange={this.onChange}
+						value={this.state.text}
+						onChange={this.onChange}
 					/>
 					<input
 						type='submit'
@@ -35,6 +44,11 @@ export class Search extends Component {
 						className='btn btn-dark btn-block'
 					/>
 				</form>
+				{showClear && (
+					<button className='btn btn-light btn-block' onClick={clearUsers}>
+						Clear
+					</button>
+				)}
 			</div>
 		);
 	}
